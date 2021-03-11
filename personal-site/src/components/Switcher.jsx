3b1942f78ebd8663a74546c77fac96bad584842d
labibahmed <12345678labib@gmail.com>
2021-03-11 10:20:28 -0500

    Changes made to expereince

--- personal-site/src/components/Switcher.jsx
+++ personal-site/src/components/Switcher.jsx
@@ -7,28 +7,32 @@ var Jobs = [{
     "Role": "Network Analyst Intern",
     "Duration": "April 2020 / August 2021",
 
-    "Description": "As a network analyst I worked in differents teams within 'Loblaw Technologies Network and Critical Facilities'." +
-        " Most of the time spent was within the automation team where we dealt automating anything within networks, I also worked with other teams suck as the forensics team where I learned about" +
-        " routers, switches, and other basic network concepts. Eventually after learning about networks I helped solve different network issues that were ticketed." +
+    "Description": "My role was within the 'Loblaw Technologies Network and Critical Facilities' where I mostly worked with the automation team." +
+        " Tasks within the automation team include creating server heath checks using ansible, automated emails/schedules using MS Power Automate, browser automation using python/selenium," +
+        " creating Power BI reports from MySQL data, learning Twillio, and more." +
         "\n\n" +
-        " Some of my tasks include automating heath checks for servers using ansible, automating emails/building schedules for other teamns using MS Power Automate, automating different configurations for virtual stores using python/selenium" +
-        ", creating Power BI reports using data gathered from MySQL database, learning Twillio to help with automated IVR systems, and more."
+        " I also worked with other teams such as the forensics team where I learned about routers and other basic network concepts." +
+        " I had to problem solve different issues reported to the forensics team" +
+        "\n\n" +
+        "Most important part about working at Loblaw was to always ask questions and to commuunicate with peers."
 }, {
     "Company": "Loblaws Supermarket",
     "Role": "Cashier/ PC Express Clerk",
     "Duration": "March 2019 / March 2020",
 
-    "Description": "As a network analyst I worked in differents teams within 'Loblaw Technologies Network and Critical Facilities'." +
-        " Most of tath checks for servers using ansible, automating emails/building schedules for other teamns using MS Power Automate, automating different configurations for virtual stores using python/selenium" +
-        ", creating Power BI reports using data gathered from MySQL database, learning Twillio to help with automated IVR systems, and more."
+    "Description": "Working as a PC Express clerk, my task was to complete grocery orders on time that were sent by customers." +
+        " Coordinating in small teams allowed my colleagues and I to provide excellent service, finish the orders, and other tasks on time." +
+        "\n\n" +
+        " Taking initiative was crucial in this environment since a lot of the time we were not under supervision of a manager, therefore whenever provided a problem my team and I would resolve it in the best way possible."
 }, {
     "Company": "Cineplex Entertainments",
     "Role": "Concession Clerk",
     "Duration": "June 2017 / August 2017",
 
-    "Description": "As a network analyst I worked in differents teams within 'Loblaw Technologies Network and Critical Facilities'." +
-        " Most of tath checks for g schedules for other teamns using MS Power Automate, automating different configurations for virtual stores using python/selenium" +
-        ", creating Power BI reports using data gathered from MySQL database, learning Twillio to help with automated IVR systems, and more."
+    "Description": "In a small span of time I was trained to provide service as a Concession Clerk. My responsibilities included interacting with customer providing food," +
+        " able to open and close the concession stand, and being able to work with cash." +
+        "\n\n" +
+        " Although the time spent at Cineplex was short, I was able to gather experience working with time constraints in a stressful and time cons environment with my colleagues."
 }]
 
 
@@ -49,7 +53,7 @@ function Switcher() {
         }}>
             <AiOutlineDoubleLeft size="30px" /></button>
 
-        <div style={{ display: "inline-block" }}>
+        <div id="exp" style={{ display: "inline-block" }}>
             <p className="experienceCompany">{Jobs.[i].Company}</p>
             <TemplateBox h2={(Jobs.[i].Role + ' - ' + Jobs.[i].Duration)} description={Jobs.[i].Description} />
         </div>
